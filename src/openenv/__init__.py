1156d896b884ad4fda9bae07015d681d1c37aba0
ben <benglickenhaus@gmail.com>
2025-11-23 10:02:49 -0500

    Initial commit: OpenEnv with local changes

---
+++ src/openenv/__init__.py
@@ -0,0 +1,12 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""OpenEnv: A unified framework for reinforcement learning environments."""
+
+# Import core so it's available as openenv.core
+from . import core
+
+__all__ = ["core"]
